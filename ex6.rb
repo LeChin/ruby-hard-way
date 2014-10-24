f9a460a88cdb70c276f6e8f3b20a4967677cf341
LeChin <elohcin@gmail.com>
2014-10-23 19:35:10 -0700

    exercise 6 and study drills

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# set types of people
+types_of_people = 10
+# use number in sentence
+x = "There are #{types_of_people} types of people."
+# set types of binary
+binary = "binary"
+# set types of do not
+do_not = "don't"
+# call two vars (string 1, 2) in sentence
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints first string w vars
+puts x
+# prints second string w vars
+puts y
+
+# print first string (in string 3)
+puts "I said: #{x}."
+# print second string (in string 4)
+puts "I also said: '#{y}'."
+
+# set hilarious to false
+hilarious = false
+# call var inside string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print string
+puts joke_evaluation
+
+# set var
+w = "This is the left side of..."
+# set var
+e = "a string with a right side."
+
+# print concatenated vars
+puts w + e
